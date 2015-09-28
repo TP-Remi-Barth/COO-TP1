cc3584c4b294be89991cf5b97351b1d93493fb65
Barthelemy Delemotte <barthelemy.delemotte@gmail.com>
2015-09-28 23:13:36 +0200

    IGameObject

---
+++ src/IGameObject.java
@@ -0,0 +1,13 @@
+
+public interface IGameObject {
+
+	/**
+	 * @return A string that describe the game object
+	 */
+	public String describe();
+	
+	/**
+	 * @return The reliable name that identify the object well
+	 */
+	public String getName();	
+}
