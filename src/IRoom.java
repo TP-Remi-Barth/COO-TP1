8afb5bf234e8244d7b397546681e23895fd528ea
Barthelemy Delemotte <barthelemy.delemotte@gmail.com>
2015-09-28 13:06:24 +0200

    + Beginning  of IRoom

---
+++ src/IRoom.java
@@ -0,0 +1,17 @@
+public interface IRoom {
+
+	public static class GoResult {
+		
+	}
+	
+	public String take(Command cmd);
+	
+	public String hit(Command cmd);
+	
+	public String describe(Command cmd);
+	
+	public String push(Command cmd);
+	
+	public String trySomething(Command cmd);
+	
+}
